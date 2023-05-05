967dab22c9c4c37f8db1a729539d4904ba514f4c
kcho760 <kcho760@gmail.com>
2023-05-05 00:31:15 -0400

    started homepage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import Example from "./scripts/example"
 
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('hello world')
-
     const main = document.getElementById('main');
     new Example(main)
 })
--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,14 +1,8 @@
-class Example {
+class InnerBox {
     constructor(htmlElement) {
-        this.htmlElement = htmlElement
-        this.htmlElement.innerHTML = "<h1>It's ALIVE!!!</h1>"
-        this.handleClick = this.handleClick.bind(this)
-        this.htmlElement.addEventListener('click', this.handleClick)
+
     }
 
-    handleClick() {
-        this.htmlElement.children[0].innerText = "Ouch"
-    }
 }
 
-export default Example;
+export default InnerBox;
