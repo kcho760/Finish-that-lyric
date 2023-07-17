7e783980645f1c340c70cabada116dc4af06c50b
kcho760 <kcho760@gmail.com>
2023-07-17 17:05:05 -0400

    Added lyrics loading

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import NextQuestionButton from "./scripts/nextQuestionButton";
 import Timer from "./scripts/timer";
 
 const state = { points: 0 };
-const numQuestions = {number: 3 };
+const numQuestions = { number: 3 };
 
-document.addEventListener('DOMContentLoaded',async () => {
+document.addEventListener('DOMContentLoaded', async () => {
   const clickMe = document.getElementById('click-me');
   const myAudio = document.getElementById('myAudio');
   clickMe.addEventListener('click', async () => {
@@ -44,21 +44,21 @@ document.addEventListener('DOMContentLoaded',async () => {
   const settingsButton = document.getElementById('settings-button');
   const settingsMenu = document.getElementById('inner-div-settings');
   settingsButton.addEventListener('click', () => {
-    if (settingsMenu.style.display !== 'block'){
-      settingsMenu.style.display = 'block'
-    }else{
-      settingsMenu.style.display = 'none'
-    };
+    if (settingsMenu.style.display !== 'block') {
+      settingsMenu.style.display = 'block';
+    } else {
+      settingsMenu.style.display = 'none';
+    }
   });
 
   const htpButton = document.getElementById('htp-button');
   const htpMenu = document.getElementById('how-to-play-menu');
   htpButton.addEventListener('click', () => {
-    if (htpMenu.style.display !== 'block'){
-      htpMenu.style.display = 'block'
+    if (htpMenu.style.display !== 'block') {
+      htpMenu.style.display = 'block';
     } else {
-      htpMenu.style.display = 'none'
-    };
+      htpMenu.style.display = 'none';
+    }
   });
 
   const scores = document.querySelectorAll('.score');
@@ -106,7 +106,7 @@ document.addEventListener('DOMContentLoaded',async () => {
   // Load lyrics as soon as DOM is loaded
   await loadLyrics();
 
-  scores.forEach(score=> {
+  scores.forEach(score => {
     score.textContent = state.points;
   });
   counters.forEach(counter => {
@@ -142,68 +142,66 @@ document.addEventListener('DOMContentLoaded',async () => {
     });
   }
 
-const nextQuestion = document.getElementById('next-question');
-    new NextQuestionButton(nextQuestion);
-    
-    nextQuestion.addEventListener("click", async () => {
-      const counter = document.querySelector(".questionCounter")
-      const numQuestionsInput = document.getElementById('number-of-questions');
-      const numQuestions = parseInt(numQuestionsInput.value);
-    
-      if (counter.textContent < numQuestions) {
-        let readyTimer = document.getElementById('ready-timer');
-        readyTimer = new Timer(5, readyTimer)
-        const readyScreen = document.querySelector(".ready-screen");
-        const innerDiv3 = document.querySelector(".inner-div3");
-        const buttons = document.querySelectorAll(".choice");
+  const nextQuestion = document.getElementById('next-question');
+  new NextQuestionButton(nextQuestion);
 
-        counters.forEach(counter => {
-          counter.textContent = parseInt(counter.textContent) + 1;
-        });
-        
-        const trackName = document.getElementById('track-name').textContent;
-        await lyrics.getNewLyrics(trackName);
-        mainTimer.time = 18;
-        readyTimer.time = 3
-        readyTimer.start();
+  nextQuestion.addEventListener("click", async () => {
+    const counter = document.querySelector(".questionCounter");
+    const numQuestionsInput = document.getElementById('number-of-questions');
+    const numQuestions = parseInt(numQuestionsInput.value);
 
-        buttons.forEach(button => {
-          button.setAttribute('data-answer', 'incorrect');
-          button.style.backgroundImage = 'linear-gradient(to top, rgb(200, 44, 62), rgb(216, 232, 77))';
-          button.disabled = false;
-        });
-        
-        setTimeout(() => {
-          readyScreen.style.display = "block";
-          innerDiv3.style.display = "none";
-        }, 100);
-        mainTimer.start();
+    if (counter.textContent < numQuestions) {
+      let readyTimer = document.getElementById('ready-timer');
+      readyTimer = new Timer(5, readyTimer);
+      const readyScreen = document.querySelector(".ready-screen");
+      const innerDiv3 = document.querySelector(".inner-div3");
+      const buttons = document.querySelectorAll(".choice");
 
-        //music switcher for next question timer
+      counters.forEach(counter => {
+        counter.textContent = parseInt(counter.textContent) + 1;
+      });
+
+      const trackName = document.getElementById('track-name').textContent;
+      await lyrics.getNewLyrics(trackName);
+      mainTimer.time = 18;
+      readyTimer.time = 3;
+      readyTimer.start();
+
+      buttons.forEach(button => {
+        button.setAttribute('data-answer', 'incorrect');
+        button.style.backgroundImage = 'linear-gradient(to top, rgb(200, 44, 62), rgb(216, 232, 77))';
+        button.disabled = false;
+      });
+
+      setTimeout(() => {
+        readyScreen.style.display = "block";
+        innerDiv3.style.display = "none";
+      }, 100);
+      mainTimer.start();
+
+      // Music switcher for the next question timer
       const audio = document.getElementById("myAudio");
       audio.src = "audio/The Countdown Clock-[AudioTrimmer.com].mp3";
       audio.loop = false;
       audio.play();
-
     } else {
-      //end screen logic
+      // End screen logic
       mainTimer.time = 15;
-      const finalScore = document.querySelector(".final-score")
-      const score = state.points
-      finalScore.textContent = `${score} / ${counter.textContent}`
+      const finalScore = document.querySelector(".final-score");
+      const score = state.points;
+      finalScore.textContent = `${score} / ${counter.textContent}`;
       const innerDiv3 = document.querySelector(".inner-div3");
       const innerDiv4 = document.querySelector(".inner-div4");
       innerDiv3.style.display = "none";
       innerDiv4.style.display = "block";
 
-      //should be audio player for on final screen
+      // Should be audio player for the final screen
       const audio = document.getElementById("myAudio");
       audio.setAttribute("src", "audio/Game Show Winner - Music for content creator (mp3cut.net).mp3");
       audio.load();
       document.getElementById('myAudio').play();
     }
   });
-  
 
   const playAgainButton = document.getElementById('play-again');
   playAgainButton.addEventListener('click', async () => {
@@ -211,22 +209,22 @@ const nextQuestion = document.getElementById('next-question');
     const counters = document.querySelectorAll('.questionCounter');
     const finalscore = document.querySelectorAll('.final-score');
     const buttons = document.querySelectorAll(".choice");
-  
+
     buttons.forEach(button => {
       button.setAttribute('data-answer', 'incorrect');
       button.style.backgroundImage = 'linear-gradient(to top, rgb(200, 44, 62), rgb(216, 232, 77))';
       button.disabled = false;
     });
-  
+
     const audio = document.getElementById("myAudio");
     audio.src = "audio/Game Show Tv Theme Music.mp3";
     audio.loop = false;
-  
+
     audio.addEventListener('canplay', () => {
       audio.play();
     });
-  
-    state.points = 0
+
+    state.points = 0;
     scores.forEach(score => {
       score.textContent = state.points;
     });
@@ -234,31 +232,29 @@ const nextQuestion = document.getElementById('next-question');
       counter.textContent = state.points;
     });
     finalscore.textContent = state.points;
-  
+
     const fiftyFifty = document.getElementById('fifty-fifty');
     fiftyFifty.removeAttribute('disabled');
     fiftyFifty.style.backgroundColor = '#d74a23';
     fiftyFifty.setAttribute('available', 'true');
-  
+
     const secondChance = document.getElementById('second-chance');
     secondChance.removeAttribute('disabled');
     secondChance.style.backgroundColor = '#d74a23';
     secondChance.setAttribute('available', 'true');
-  
+
     const innerDiv4 = document.querySelector('.inner-div4');
     const innerDiv1 = document.querySelector('.inner-div1');
     innerDiv4.style.display = 'none';
     innerDiv1.style.display = 'flex';
-  
+
     // Remove event listener from second chance button
     const clonedSecondChance = secondChance.cloneNode(true);
     secondChance.parentNode.replaceChild(clonedSecondChance, secondChance);
-  
+
     // Create a new instance of the SecondChance class
     const secondChanceButton = new SecondChance(clonedSecondChance);
-  
+
     await lyrics.getNewLyrics();
   });
-  
-  
-})
+});
