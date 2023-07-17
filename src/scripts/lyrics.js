f62f7f99a7c58325a802c6893141499d35bd4707
kcho760 <kcho760@gmail.com>
2023-07-17 13:38:33 -0400

    Answer fixes

--- src/scripts/lyrics.js
+++ src/scripts/lyrics.js
@@ -1,91 +1,104 @@
-import axios from 'axios';
-
-//key1: c335682791d58fcd23f5d30abbc72d34
-//key2: ebd7f9988730ec4067168ec23e60f3e7
-//key3: eca0bf1d91cfe339d5cf01290bca48db
+//snippet version
 class Lyrics {
   constructor() {
   }
+//key1: c335682791d58fcd23f5d30abbc72d34
+//key2: ebd7f9988730ec4067168ec23e60f3e7
+//key3: eca0bf1d91cfe339d5cf01290bca48db
 
   async getNewLyrics() {
     const apikey = 'c335682791d58fcd23f5d30abbc72d34';
     const artistName = document.getElementById('api-input').value;
     const artistUrl = encodeURIComponent(artistName);
     const chartUrl = `https://proxy-92z3.onrender.com/?url=https%3A%2F%2Fapi.musixmatch.com%2Fws%2F1.1%2Ftrack.search%3Fq_artist%3D${artistName}%26page_size%3D15%26s_track_rating%3Ddesc%26apikey%3D${apikey}`;
+    
+    fetch(chartUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to retrieve top tracks: ${response.status} ${response.statusText}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        const tracks = data.message.body.track_list;
+        const randomIndex = Math.floor(Math.random() * tracks.length);
+        const track_id = tracks[randomIndex].track.track_id;
+        this.artistName = tracks[randomIndex].track.artist_name;
+        this.trackName = tracks[randomIndex].track.track_name;
+        document.getElementById("artist-name").textContent = `Artist: ${this.artistName}`;
+        document.getElementById("track-name").textContent = `Track: ${this.trackName}`;
+        const snippetsUrl = `https://proxy-92z3.onrender.com/?url=https%3A%2F%2Fapi.musixmatch.com%2Fws%2F1.1%2Ftrack.snippet.get%3Ftrack_id%3D${track_id}%26apikey%3D${apikey}`;
+        return fetch(snippetsUrl);
+      })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Failed to retrieve snippets');
+        }
+        return response.json();
+      })
+      .then(async data => {
+        const snippets = data.message.body.snippet.snippet_body.split("\n");
+        const filteredSnippets = snippets.filter(snippet => !snippet.includes("This Lyrics is NOT for Commercial use") && !snippet.endsWith("..."));
+        const randomIndex = Math.floor(Math.random() * filteredSnippets.length);
+        const wordsToReplace = 1;
+        const answerWords = [];
+        const words = filteredSnippets[randomIndex].split(' ');
+        const commonWords = ["I","what", "a", "an", "the",
+         "and", "that", "in", "you", "me", "is",
+          "do", "can", "just", "of", "to", "for",
+           "on", "with", "at", "by", "from", "up",
+            "down", "out", "about", "my", "I'm"
+          ,"oh","ooh","oooh","ooooh","oooooh","ooooooh"];
 
-    try {
-      const response = await axios.get(chartUrl, { timeout: 10000 });
-      const data = response.data;
-      const tracks = data.message.body.track_list;
-      const randomIndex = Math.floor(Math.random() * tracks.length);
-      const track_id = tracks[randomIndex].track.track_id;
-      this.artistName = tracks[randomIndex].track.artist_name;
-      this.trackName = tracks[randomIndex].track.track_name;
-      document.getElementById("artist-name").textContent = `Artist: ${this.artistName}`;
-      document.getElementById("track-name").textContent = `Track: ${this.trackName}`;
-      const snippetsUrl = `https://proxy-92z3.onrender.com/?url=https%3A%2F%2Fapi.musixmatch.com%2Fws%2F1.1%2Ftrack.snippet.get%3Ftrack_id%3D${track_id}%26apikey%3D${apikey}`;
-
-      const snippetResponse = await axios.get(snippetsUrl, { timeout: 5000 });
-      const snippetData = snippetResponse.data;
-      const snippets = snippetData.message.body.snippet.snippet_body.split("\n");
-      const filteredSnippets = snippets.filter(snippet => !snippet.includes("This Lyrics is NOT for Commercial use") && !snippet.endsWith("..."));
-
-      const randomIndexSnippet = Math.floor(Math.random() * filteredSnippets.length);
-      const wordsToReplace = 1;
-      const answerWords = [];
-      const words = filteredSnippets[randomIndexSnippet].split(' ');
-      const commonWords = ["I", "what", "a", "an", "the", "and", "that", "in", "you", "me", "is", "do", "can", "just", "of", "to", "for", "on", "with", "at", "by", "from", "up", "down", "out", "about", "my", "I'm", "oh", "ooh", "oooh", "ooooh", "oooooh", "ooooooh"];
-
-      for (let i = 0; i < wordsToReplace; i++) {
-        let selectedWord;
-        let randomWordIndex;
-        while (true) {
-          randomWordIndex = Math.floor(Math.random() * words.length);
-          selectedWord = words[randomWordIndex];
-          if (!commonWords.includes(selectedWord) && selectedWord.length >= 4) {
-            break;
+        for (let i = 0; i < wordsToReplace; i++) {
+          let selectedWord;
+          let randomWordIndex;
+          while (true) {
+            randomWordIndex = Math.floor(Math.random() * words.length);
+            selectedWord = words[randomWordIndex];
+            if (!commonWords.includes(selectedWord) && selectedWord.length >= 4) {
+              break;
+            }
           }
-        }
-        answerWords.push({ word: selectedWord, index: randomWordIndex });
-        words[randomWordIndex] = "____";
-      }
-
-      const question = words.join(' ');
-      const lyricsText = `${question}`;
-      document.getElementById("lyrics").innerHTML = lyricsText;
-      const buttons = [document.getElementById("button1"), document.getElementById("button2"), document.getElementById("button3"), document.getElementById("button4")];
-      const answerButtonIndex = Math.floor(Math.random() * 4);
-      const answerWordsText = answerWords.map(wordObj => wordObj.word).join(', ');
-      buttons[answerButtonIndex].textContent = answerWordsText.toLowerCase();
-      buttons[answerButtonIndex].setAttribute("data-answer", "correct");
-      const usedWords = [];
-      const incorrectButtons = buttons.filter((button, index) => index !== answerButtonIndex);
-
-      const promises = incorrectButtons.map(async (button) => {
-        const response = await axios.get(`https://api.datamuse.com/words?ml=${answerWords[0].word}&max=100&min=4`);
-        const words = await response.data;
-
-        const availableWords = words.filter(word => !usedWords.includes(word.word));
-
-        if (availableWords.length === 0) {
-          usedWords.length = 0;
-          return;
+          answerWords.push({ word: selectedWord, index: randomWordIndex });
+          words[randomWordIndex] = "____";
         }
 
-        const randomWord = availableWords[Math.floor(Math.random() * availableWords.length)];
-        button.textContent = randomWord.word;
-        button.setAttribute("data-answer", "incorrect");
+        const question = words.join(' ');
+        const lyricsText = `${question}`;
 
-        usedWords.push(randomWord.word);
+        document.getElementById("lyrics").innerHTML = lyricsText;
+        const buttons = [document.getElementById("button1"), document.getElementById("button2"), document.getElementById("button3"), document.getElementById("button4")];
+        const answerButtonIndex = Math.floor(Math.random() * 4); 
+        const answerWordsText = answerWords.map(wordObj => wordObj.word).join(', ');
+        buttons[answerButtonIndex].textContent = answerWordsText.toLowerCase();
+        buttons[answerButtonIndex].setAttribute("data-answer", "correct");
+        const usedWords = [];
+
+        const incorrectButtons = buttons.filter((button, index) => index !== answerButtonIndex);
+
+        const promises = incorrectButtons.map(async (button) => {
+          const response = await fetch(`https://api.datamuse.com/words?ml=${answerWords[0].word}&max=100&min=4`);
+          const words = await response.json();
+
+          const availableWords = words.filter(word => !usedWords.includes(word.word));
+
+          if (availableWords.length === 0) {
+            usedWords.length = 0;
+            return;
+          }
+
+          const randomWord = availableWords[Math.floor(Math.random() * availableWords.length)];
+          button.textContent = randomWord.word;
+          button.setAttribute("data-answer", "incorrect");
+
+          usedWords.push(randomWord.word);
+        });
+
+        await Promise.all(promises).catch(error => {
+          console.error('Error:', error);
+        });
       });
-
-      await Promise.all(promises).catch(error => {
-        console.error('Error:', error);
-      });
-
-    } catch (error) {
-      console.error('Error:', error);
-    }
   }
 }
 
